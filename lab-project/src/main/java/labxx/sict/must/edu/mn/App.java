5238382e2ec91b3493e7035d802939103be580ab
nmndari <namukbd@gmail.com>
2025-05-03 22:52:45 +0800

    "Maven-д суурилсан Java тооцоолуурын төсөл эхлүүлэх

---
+++ lab-project/src/main/java/labxx/sict/must/edu/mn/App.java
@@ -0,0 +1,10 @@
+package labxx.sict.must.edu.mn;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
