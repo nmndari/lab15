ac5cea192a32427ec5f2330e830977096b2e1eb2
nmndari <namukbd@gmail.com>
2025-05-09 15:21:09 +0800

    Update package

--- lab-project/src/main/java/labxx/sict/must/edu/mn/package-info.java
+++ lab-project/src/main/java/labxx/sict/must/edu/mn/package-info.java
@@ -2,8 +2,8 @@
  * This package contains utility classes for mathematical operations.
  * 
  * <p>
- * The {@code Division} class provides methods to perform division while handling
- * special cases like division by zero.
+ * The {@code Division} class provides methods to perform division 
+ * while handling special cases like division by zero.
  * </p>
  */
 package labxx.sict.must.edu.mn;
