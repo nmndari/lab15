040f50dc1e8dfde0477404feb02980305b481047
nmndari <namukbd@gmail.com>
2025-05-09 16:06:19 +0800

    Таны commit мессеж

---
+++ lab-project/src/main/java/labxx/sict/must/edu/mn/Division.java
@@ -0,0 +1,23 @@
+package labxx.sict.must.edu.mn;
+
+public final class Division {
+
+    private Division() {
+        // prevent instantiation
+    }
+    /**
+     * Divides two integers.
+     *
+     * @param a the dividend
+     * @param b the divisor
+     * @return the result of the division
+     * @throws IllegalArgumentException if b is zero
+     */
+    public static int divide(final int a, final int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+}
+
